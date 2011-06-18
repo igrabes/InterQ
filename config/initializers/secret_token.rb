4b8e1a4417ae575fd9646e4df10dcd5bf1e6c8b0
Ian <ian.grabill@gmail.com>
2011-06-17 20:04:27 -0400

    first commit of INTERQ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interq::Application.config.secret_token = '408a72f03e5e70907b59afb1db9477a2b9c8ae2f1d7e40ffa1f0635a081140b4c37120c0335cd9ef81f27deb88f891ed951c2a9af44c10f947b55ac57e8e814a'
